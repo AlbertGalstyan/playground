b6a92966cf098c638de2571fcc3567f3a6cdddf6
unknown <albert.galstyan01@gmail.com>
2024-09-19 17:08:49 +0400

    added dashboard page

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,3 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
 class Dashboard:
     def __init__(self, driver):
-        pass
+        self.driver = driver
+        self.header_element = (By.TAG_NAME, "h1")
+
+    def get_header_element(self):
+        header = self.driver.find_element(*self.header_element)
+        return header.text
